c39842a6d5362681c9f21e1d2f97dbd890c1fdad
koominjun <mjkoo711@gmail.com>
2019-05-20 17:39:18 +0900

    init for base

--- RxSwiftExample/ViewController.swift
+++ RxSwiftExample/ViewController.swift
@@ -11,16 +11,9 @@ import RxSwift
 import RxCocoa
 
 class ViewController: UIViewController {
-  @IBOutlet var searchBar: UISearchBar!
-  @IBOutlet var tableView: UITableView!
-
-
-  var showCities: [String] = []
-  var allCities = ["New York", "London", "Oslo", "Warsaw", "Berlin", "Praga"]
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    tableView.dataSource = self
   }
 
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
@@ -28,19 +21,3 @@ class ViewController: UIViewController {
   }
 
 }
-
-extension ViewController: UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return showCities.count
-  }
-
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "CityTableCell", for: indexPath)
-    cell.textLabel?.text = showCities[indexPath.row]
-
-    return cell
-  }
-
-
-}
-
